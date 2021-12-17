eac6989247ca1240e1c051333bfef72f809182be
Žygimantas Kadiša <kadisa917@gmail.com>
2021-12-17 17:48:51 +0200

    Day 16 initial

---
+++ 16/main_pt1.py
@@ -0,0 +1,81 @@
+data = open("input_test.txt", 'r').readlines()
+hexData = [int(x, 16) for x in data[0]]
+binaryData = [x for x in "".join(["{0:04b}".format(h) for h in hexData])]
+
+class Packet:
+    def __init__(self, version, typeID):
+          self.version = version
+          self.typeID = typeID
+      
+
+class Literal(Packet):  
+    def __init__(self, version, typeID, literal):
+          self.version = version
+          self.typeID = typeID
+          self.literal = literal
+
+class Operator(Packet): 
+    def __init__(self, version, typeID, packets):
+          self.version = version
+          self.typeID = typeID
+          self.packets = packets
+
+    def SumVersions(self):
+          return self.version
+      
+
+def ReadBinary(data, length):
+      binary = data[0:length]
+      del data[0:length]
+      return "".join(binary)
+      
+
+def ReadBinaryAsInt(data, length):
+      return int("".join(ReadBinary(data, length)), 2)
+
+def ReadLiteral(data):
+      literalString = ""
+      while ReadBinaryAsInt(data, 1) == 1:
+            literalString += ReadBinary(data, 4)
+      literalString += ReadBinary(data, 4)
+      return int(literalString, 2)
+            
+def ReadOperator(data):
+      packets = []
+      lengthTypeID = ReadBinaryAsInt(data, 1)
+
+      if lengthTypeID == 0:
+            subPacketLength = ReadBinaryAsInt(data, 15)
+            print("lengthTypeID: " + str(lengthTypeID) + "; subPacketLength: " + str(subPacketLength))
+
+            subpacketsData = data[0:subPacketLength]
+            del data[0:subPacketLength]
+            while len(subpacketsData) > 0:
+                  packets.extend(ReadPacket(subpacketsData))
+      else:           
+            subpacketCount = ReadBinaryAsInt(data, 11)     
+            print("lengthTypeID: " + str(lengthTypeID) + "; subpacketCount: " + str(subpacketCount))
+
+            for i in range(0, subpacketCount):
+                  packets.extend(ReadPacket(data))
+      return packets
+      
+
+def ReadPacket(data):
+      packets = []
+      version = ReadBinaryAsInt(data, 3)
+      typeID = ReadBinaryAsInt(data, 3)
+
+      print("version: " + str(version) + "; typeID: " + str(typeID))
+      
+      if typeID == 4:
+            literal = ReadLiteral(data)
+            print("literal: " + str(literal))
+            packets.append(Literal(version, typeID, literal))
+      else:
+            packets.append(Operator(version, typeID, ReadOperator(data)))
+
+      return packets
+
+packets = ReadPacket(binaryData)
+print(packets)
---
+++ 16/main_pt2.py
@@ -0,0 +1,85 @@
+data = open("input.txt", 'r').readlines()
+templateMaze = [[int(i) for i in x.rstrip()] for x in data]
+
+def printMatrix(matrix):
+  print('\n'.join([''.join(['{:1}'.format(item) for item in row]) 
+    for row in matrix]))
+
+def clamp(x, k):
+    return (x + k - 1) % 9 + 1
+
+maze = [row[:] for row in templateMaze]
+for y in range(0, len(maze)):
+    for i in range(0, 4):
+        maze[y].extend([clamp(x, i + 1) for x in templateMaze[y]])
+templateMaze = [row[:] for row in maze]
+
+for i in range(0, 4):
+    maze.extend([[clamp(x, i + 1) for x in row] for row in templateMaze])
+
+def printOpen(maze, openList):
+  print('\n'.join([''.join(['{:1}'.format('+' if [y,x] in openList else '.') for x in range(0, len(maze[0]))]) 
+    for y in range(0, len(maze))]))
+
+def Backtrack(pos, maze, cameFrom):
+  path = [pos]
+  while pos != [0, 0]:
+    path.append(cameFrom[pos[0]][pos[1]])
+    pos = cameFrom[pos[0]][pos[1]]
+  return path[::-1]
+
+def printPath(path, maze):
+  print('\n'.join([''.join(['{:1}'.format('+' if [y,x] in path else '.') for x in range(0, len(maze[0]))]) 
+    for y in range(0, len(maze))]))
+
+maxY = len(maze)
+maxX = len(maze[0])
+
+openList = []
+closedList = []
+cameFrom = [[[] for x in range(0, maxX)] for y in range(0, maxY)]
+riskUpTo = [[999999 for x in range(0, maxX)] for y in range(0, maxY)]
+riskUpTo[0][0] = 0
+
+openList.append([0, 0])
+
+while len(openList) > 0:
+  current = min(openList, key = lambda x: riskUpTo[x[0]][x[1]])
+
+  if current == [maxY - 1, maxX - 1]:
+    print("found path from [0, 0] to " + str(current))
+    break
+
+  openList.remove(current)
+  closedList.append(current)
+
+  currentRisk = riskUpTo[current[0]][current[1]]
+
+  for i in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+    childIndex = [current[0] + i[0], current[1] + i[1]]
+
+    if childIndex in closedList:
+      continue
+
+    if (childIndex[0] < 0 or childIndex[0] > maxY - 1 or
+        childIndex[1] < 0 or childIndex[1] > maxX - 1):
+      continue
+
+    risk = currentRisk + maze[childIndex[0]][childIndex[1]]
+    if riskUpTo[childIndex[0]][childIndex[1]] <= risk:
+      continue
+
+    openList.append(childIndex)
+    cameFrom[childIndex[0]][childIndex[1]] = [current[0], current[1]]
+    riskUpTo[childIndex[0]][childIndex[1]] = risk
+  
+  #printOpen(maze, openList)
+  #print('\n\n')
+        
+#printMatrix(maze)
+
+#path = Backtrack([maxY - 1, maxX - 1], maze, cameFrom)
+#printPath(path, maze)
+#path.remove([0,0])
+risk = riskUpTo[maxY - 1][maxX - 1]
+print(risk)
