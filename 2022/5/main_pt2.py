0b7b3847d4e32b543cce83ff2d352eae55809cdc
Žygimantas Kadiša <kadisa917@gmail.com>
2022-12-06 19:18:22 +0200

    2022: Day 5

---
+++ 2022/5/main_pt2.py
@@ -0,0 +1,25 @@
+input_file = 'input.txt'
+
+raw_input = open(input_file).read().split('\n')
+stacks_data_string = raw_input[:raw_input.index('') - 1]
+
+stacks_data = list(reversed([list(stack[i:i+3] for i in range(0, len(stack), 4)) for stack in stacks_data_string]))
+stacks = [[] for i in range(0, len(stacks_data[0])) ]
+
+for stack in stacks_data:
+    for i in range(0, len(stack)):
+        if stack[i].strip(' ') != '':
+            stacks[i].append(stack[i])
+
+for command in raw_input[raw_input.index('')+1:]:
+    parts = command.split(' ')
+    count = int(parts[1])
+    s1 = int(parts[3]) - 1
+    s2 = int(parts[5]) - 1
+    for i in range(0, count):
+        stacks[s2].append(stacks[s1][len(stacks[s1]) - (count - i)])
+    for i in range(0, count):
+        stacks[s1].pop()
+
+top_most_crates = ''.join([(s[len(s) - 1]).strip('[]') for s in stacks])
+print(top_most_crates)
