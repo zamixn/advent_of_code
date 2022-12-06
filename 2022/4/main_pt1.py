dcf9e3472de5f5519ae7dea51b63be22d14b703f
Žygimantas Kadiša <kadisa917@gmail.com>
2022-12-06 19:18:12 +0200

    2022: Day 4

---
+++ 2022/4/main_pt1.py
@@ -0,0 +1,10 @@
+input_file = 'input.txt'
+
+def IsOverlapping(pair):
+    return (pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]) or (pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1])
+
+pairs_raw = [x.split(',') for x in open(input_file).read().split('\n')]
+pairs = [[[int(x) for x in p[0].split('-')], [int(x) for x in p[1].split('-')]] for p in pairs_raw]
+
+overlapping = sum(map(IsOverlapping, pairs))
+print(overlapping)
